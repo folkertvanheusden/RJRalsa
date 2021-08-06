8f34a4c1ff8ebcd0d9fc21cb5368341aa5c0dbb6
folkert van heusden <mail@vanheusden.com>
2021-08-06 23:28:54 +0200

    Handle multiple ports

--- RJRalsa.py
+++ RJRalsa.py
@@ -23,19 +23,13 @@ ppqn = 64
 # minimum size of output
 min_size = 0
 
-rtmidi = mido.Backend('mido.backends.rtmidi')
-input_ = rtmidi.open_input('RJRalsa', virtual=True)
-
-def listener(q, input_):
-    while True:
-        m = input_.receive()
-        t = time.time()
-
-        q.put((m, t))
-
-def start_file():
+def start_file(port):
     tm = time.localtime()
-    name = f'recording_{tm.tm_year}-{tm.tm_mon:02d}-{tm.tm_mday:02d}_{tm.tm_hour:02d}-{tm.tm_min:02d}-{tm.tm_sec:02d}.mid'
+
+    port = port.replace(':', '-')
+    port = port.replace(' ', '-')
+
+    name = f'recording_{tm.tm_year}-{tm.tm_mon:02d}-{tm.tm_mday:02d}_{tm.tm_hour:02d}-{tm.tm_min:02d}-{tm.tm_sec:02d}_{port}.mid'
 
     track = mido.MidiTrack()
 
@@ -57,6 +51,93 @@ def end_file(pars):
 
 def t_to_tick(ts, p_ts):
     return int(mido.second2tick(ts - p_ts, ppqn, mido.bpm2tempo(bpm)))
+
+def port_listener(q, input_):
+    while True:
+        try:
+            m = input_.receive()
+            t = time.time()
+
+            q.put((m, t))
+
+        except:  # FIXME
+            q.put(None)
+            break
+
+    print('port_listener stops')
+
+def port_handler(rtmidi, port):
+    input_ = rtmidi.open_input(port)
+
+    print(f'Opened port "{port}"')
+
+    q = queue.Queue()
+
+    t = threading.Thread(target=port_listener, args=(q, input_,))
+    t.start()
+
+    state = None
+
+    while True:
+        # end file after x seconds of silence
+        if state and time.time() - state['latest_msg'] >= inactivity:
+            end_file(state['file'])
+            print(f"{time.ctime()}] File {state['file'][1]} ended")
+            state = None
+            continue
+
+        try:
+            item = q.get(timeout=0.5)
+
+        except queue.Empty:
+            continue
+
+        except KeyboardInterrupt:
+            print('Terminating application')
+            if state:
+                end_file(state['file'])
+            break
+
+        if not item:
+            if state:
+                end_file(state['file'])
+            state = None
+            break
+
+        if state == None:
+            state = dict()
+            state['latest_msg'] = state['started_at'] = item[1]
+            state['file'] = start_file(port)
+            print(f"{time.ctime()}] Started recording to {state['file'][1]}")
+
+        item[0].time = t_to_tick(item[1], state['latest_msg'])
+        print(item)
+        state['file'][0].append(item[0])
+
+        state['latest_msg'] = item[1]
+
+    input_.close()
+
+    print(f'Port "{port}" closed')
+
+def port_waiter(rtmidi):
+    state = dict()
+
+    while True:
+        ports = rtmidi.get_input_names()
+
+        for p in ports:
+            if not p in state:
+                print(f'Starting thread for port "{p}"')
+                state[p] = dict()
+                state[p]['th'] = threading.Thread(target=port_handler, args=(rtmidi, p,))
+                state[p]['th'].start()
+
+        for p in ports:
+            if state[p]['th'].is_alive() == False:
+                state[p]['th'].join()
+
+        time.sleep(1.0/3)
 
 def usage():
     print('-i x   inactivity timer (in seconds, optional)')
@@ -93,53 +174,10 @@ for o, a in opts:
         usage()
         assert False, "unhandled option"
 
-state = None
+rtmidi = mido.Backend('mido.backends.rtmidi')
 
-q = queue.Queue()
-
-t = threading.Thread(target=listener, args=(q, input_,))
-t.start()
-
-while True:
-    # end file after x seconds of silence
-    if state and time.time() - state['latest_msg'] >= inactivity:
-        end_file(state['file'])
-        print(f"{time.ctime()}] File {state['file'][1]} ended")
-        state = None
-        continue
-
-    try:
-        item = q.get(timeout=0.5)
-
-    except queue.Empty:
-        continue
-
-    except KeyboardInterrupt:
-        print('Terminating application')
-        if state:
-            end_file(state['file'])
-        break
-
-    if not item:
-        if state:
-            end_file(state['file'])
-        state = None
-        break
-
-    if state == None:
-        state = dict()
-        state['latest_msg'] = state['started_at'] = item[1]
-        state['file'] = start_file()
-        print(f"{time.ctime()}] Started recording to {state['file'][1]}")
-
-    item[0].time = t_to_tick(item[1], state['latest_msg'])
-    print(item)
-    state['file'][0].append(item[0])
-
-    state['latest_msg'] = item[1]
-
-input_.close()
-
-t.join()
+port_waiter_th = threading.Thread(target=port_waiter, args=(rtmidi,))
+port_waiter_th.start()
+port_waiter_th.join()
 
 sys.exit(0)
